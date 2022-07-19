bc5fb6000a9769b997a014eb80bb7dc23795c9e2
Braian Troncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-07-19 01:50:19 -0300

    Add files via upload
    Create a While loop, this loop must have as a condition that the variable numberWhile is less than 3, the code block that will have the loop must:
    
    Increment the value of the variable by one each time it is executed.
    
    Show it on the screen every time it is executed.

---
+++ w_hile.java
@@ -0,0 +1,14 @@
+public class w_hile {
+
+  public static void main(String[] args) {
+      int numWhile = 0;
+
+      while (numWhile < 3){
+          System.out.println("While: "+numWhile);
+          numWhile++;
+      }
+  }
+
+}
+
+
