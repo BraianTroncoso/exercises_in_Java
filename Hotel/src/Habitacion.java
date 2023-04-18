7ab8759055ce53c298eb871092de4c35156f4ad5
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-04-18 05:06:33 -0300

    [ADD] Ejercicio OOP Hotel acc Consigna

--- Hotel/src/Habitacion.java
+++ Hotel/src/Habitacion.java
@@ -1,3 +1,17 @@
+/*
+Cree una clase llamada Hábitacion que inclua un atributo entero para el numero de la habitacion
+y un atributo real para la tarifa de alquiler nocturno. Inclua métodos get para estos campos
+y un constructor que requiera un argumento entero que represente el número de hábitacion. El
+constructor establece la tarifa de la habitación según el número de hábitacion. El constructor
+establece la tarifa de la habitacion según el número de la habitacion, las habitaciones numeradas
+299 y siguientes cuestan $69.95 por noche, y las otras cuestan $89.95 por noche.
+Cree una clase extendida llmada Suite cuyo constructor requiere número de habitación y agrega un recargo
+de $30 a la tarifa de la habitacion regular del hotel, que nuevamente se basa en el numero de habitacion
+Escriba una aplicacion llamada Prinabitacion que crea un objeto de cada clasey demuestre que todos
+los métodos funcionan correctamente Guarde los archivos como Habitacion.java, Suite.java, PrintHabitacion.java
+ */
+
+
 public class Habitacion {
 
     protected int numHabitacion;
